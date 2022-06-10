78976f3f770a62287889d1d223286f4e73d15ef9
galiniec <clement.galinier@etu.univ-grenoble-alpes.fr>
2022-06-10 13:27:30 +0200

    avancement prototypage map

---
+++ game/View/TestMap.java
@@ -0,0 +1,30 @@
+package View;
+
+public class TestMap {
+
+	public static void main (String[] args) {
+		int carte[][] = new int[2][2];
+		carte[0][0] = 0;
+		carte[0][1] = 1;
+		carte[1][0] = 2;
+		carte[1][1] = 3;
+		int carte2[][] = new int[7][7];
+		
+		
+
+		mapView var = new mapView(carte);
+		mapView var2 = new mapView(carte2);
+		int res = var.hauteur();
+		System.out.println("hauteur"+res);
+		res = var.largeur();
+		System.out.println("largeur"+res);
+		res = var2.hauteur();
+		System.out.println("hauteur"+res);
+		res = var2.largeur();
+		System.out.println("largeur"+res);
+		
+		var.paint(null);
+
+	}
+
+}
--- game/View/mapView.java
+++ game/View/mapView.java
@@ -1,5 +1,6 @@
 package View;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
 
@@ -8,16 +9,26 @@ public class mapView extends ViewElement {
 	//ICI je pars du principe que la map est affiché par un tableau a deux dimensions de int. Dans le cas ou "carte[][]" est pour une salle seulement alors ont fait une liste de ces salles là.
 	int carte[][];
 	int nb_salle;
+	int x = 1;
+	int y = 1;
+	final int l_case = 50;
+	final int h_case = 50;
+	int h;
+	int l;
 
 
-	int hauteur (int m[][]) {
-		return m.length;
+	public mapView (int carte[][]) {
+		this.carte = carte;
 	}
 
-	int largeur (int m[][]) {
+	int hauteur () {
+		return this.carte.length;
+	}
+
+	int largeur () {
 
 		try {
-			return m[0].length;
+			return this.carte[0].length;
 		}
 		catch (IllegalArgumentException e) {
 			System.out.println("Something went wrong.");
@@ -27,7 +38,27 @@ public class mapView extends ViewElement {
 
 	@Override
 	void paint (Graphics g) {
+
 		// TODO Auto-generated method stub
+		switch (carte[h][l]) {
+			case 0:
+				//noir;
+				g.drawRect(x * h, y * l, l_case, h_case);
+				g.setColor(Color.black);
+			case 1:
+				//orange;
+				g.drawRect(x * h, y * l, l_case, h_case);
+				g.setColor(Color.orange);
+			case 2:
+				//vert
+				g.drawRect(x * h, y * l, l_case, h_case);
+				g.setColor(Color.green);
+			default:
+				//gris
+				g.drawRect(x * h, y * l, l_case, h_case);
+				g.setColor(Color.gray);
+
+		}
 
 	}
 
