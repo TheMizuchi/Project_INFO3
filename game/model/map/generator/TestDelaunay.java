e363f648a5284e89930606d2d47e11c79e6c12d0
Théo Lopes <theo.lopes1@etu.univ-grenoble-alpes.fr>
2022-06-14 12:14:20 +0200

    File of Test for delaunay algo

---
+++ game/model/map/generator/TestDelaunay.java
@@ -0,0 +1,19 @@
+package model.map.generator;
+
+import info3.game.Game;
+
+
+public class TestDelaunay {
+
+	public static void main (String args[]) throws Exception {
+		System.out.println("Testing Circumscribed circle...");
+		Node A = new Node(0, 0);
+		Node B = new Node(2, 0);
+		Node C = new Node(0, 2);
+		Triangle tri = new Triangle(A, B, C);
+
+		if (false) {
+
+		}
+	}
+}
