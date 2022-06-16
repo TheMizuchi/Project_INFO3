6cbb45ed07cf86663d2f50c0b475747239f51efa
Killian Gricourt <gricour.killian@gmail.com>
2022-06-16 09:41:27 +0200

    gestion du zoom en fonction de la distance entre les deux joueurs

--- game/model/Camera.java
+++ game/model/Camera.java
@@ -1,5 +1,6 @@
 package model;
 
+import model.entity.Entity;
 import model.entity.EntityInterface;
 import view.Viewport;
 
@@ -9,6 +10,12 @@ public class Camera {
 	private EntityInterface j1;
 	private EntityInterface j2;
 	
+	public Camera(Viewport vp, Entity j1, Entity j2) {
+		this(vp);
+		this.j1 = j1;
+		this.j2 = j2;
+	}
+	
 	public Camera(Viewport vp) {
 		this.vp = vp;
 	}
@@ -16,8 +23,16 @@ public class Camera {
 	public void update() {
 		double dx = Math.abs(j1.getPosX()-j2.getPosX());
 		double dy = Math.abs(j1.getPosY()-j2.getPosY());
-		double scale = 1;
+		double scale = Math.max(Math.max(Math.max(dx, 13)/13, Math.max(dy, 9)/9), 1);
 		this.setPosition(dx/2, dy/2, scale);
+	}
+	
+	public void setj1(Entity j) {
+		this.j1 = j;
+	}
+	
+	public void setj2(Entity j) {
+		this.j2 = j;
 	}
 	
 	//passer en private quand on aura les deux joueurs
