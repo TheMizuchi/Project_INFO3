39754a91455e8e2996ca4d50272a58c7556845e9
galiniec <clement.galinier@etu.univ-grenoble-alpes.fr>
2022-06-16 11:37:38 +0200

    début j1

--- game/view/animation/bank/J1Bank.java
+++ game/view/animation/bank/J1Bank.java
@@ -1,6 +1,42 @@
 package view.animation.bank;
 
+import view.animation.Sprite;
 
-public class J1Bank {
+
+public class J1Bank extends EntityAnimationBank {
+
+	//Liste des aniations spécifique au J1;
+	public Sprite attack;
+
+
+	private J1Bank () {
+		Sprite spriteFile = Sprite.loadSprite("resources/winchester-4x6.png", 4, 6);
+		loadBasicAnimation(spriteFile);
+		loadSpecificAnimation(spriteFile);
+	}
+
+
+	private static J1Bank INSTANCE = null;
+
+
+	public static J1Bank getInstance () {
+
+		if (INSTANCE == null) {
+			INSTANCE = new J1Bank();
+		}
+		return INSTANCE;
+	}
+
+	@Override
+	protected void loadBasicAnimation (Sprite spriteFile) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void loadSpecificAnimation (Sprite spriteFile) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
--- game/view/graphicEntity/J1View.java
+++ game/view/graphicEntity/J1View.java
@@ -1,6 +1,79 @@
 package view.graphicEntity;
 
+import java.awt.Graphics;
 
-public class J1View {
+import model.entity.EntityInterface;
+import view.EntityView;
+import view.MyCanvas;
+import view.animation.Animation;
+import view.animation.J1Animation;
+import view.animation.Animation.AnimationListener;
+import view.animation.bank.AnimationBank;
+import view.animation.bank.J1Bank;
+
+
+public class J1View extends EntityView {
+
+	AnimationListener al;
+	J1Animation a;
+
+
+	public J1View (EntityInterface e) {
+		super(0, 0, 1, e);
+		this.a = new J1Animation();
+		a.setPosition(x, y, scale);
+		this.al = new AnimationListener() {
+
+			@Override
+			public void done (Animation a) {
+
+			}
+		};
+	}
+
+	@Override
+	public void setPosition (int x, int y, double scale) {
+		this.x = x;
+		this.y = y;
+		this.scale = scale;
+		a.setPosition(x, y, scale);
+	}
+
+	public void setDelay (int delay) {
+		a.setDelay(delay);
+	}
+
+	public void attack () {
+		a.setListener(this.al);
+		a.attack();
+	}
+
+	@Override
+	public int getH () {
+		return this.a.getH();
+	}
+
+	@Override
+	public int getW () {
+		return this.a.getW();
+	}
+
+	@Override
+	public void turnLeft () {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void turnRight () {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void paint (Graphics g) {
+		a.paint(g);
+
+	}
 
 }
