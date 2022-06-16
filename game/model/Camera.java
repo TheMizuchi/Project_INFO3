79ca6520241ac53eedc4b2ca8ace5ddc830e6ffe
Killian Gricourt <gricour.killian@gmail.com>
2022-06-16 09:27:29 +0200

    Maj camera

--- game/model/Camera.java
+++ game/model/Camera.java
@@ -1,20 +1,27 @@
 package model;
 
+import model.entity.EntityInterface;
 import view.Viewport;
 
 public class Camera {
 	
 	private Viewport vp;
+	private EntityInterface j1;
+	private EntityInterface j2;
 	
 	public Camera(Viewport vp) {
 		this.vp = vp;
 	}
 	
-	void setPosition(double x, double y) {
-		this.vp.setPosition(x, y);
+	public void update() {
+		double dx = Math.abs(j1.getPosX()-j2.getPosX());
+		double dy = Math.abs(j1.getPosY()-j2.getPosY());
+		double scale = 1;
+		this.setPosition(dx/2, dy/2, scale);
 	}
 	
-	void setPosition(double x, double y, double scale) {
+	//passer en private quand on aura les deux joueurs
+	public void setPosition(double x, double y, double scale) {
 		this.vp.setPosition(x, y, scale);
 	}
 }
