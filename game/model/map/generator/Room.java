d104ee30655b1629897cb0e34a07a50745add8c0
Mathis Courant <Mathis Courant>
2022-06-17 17:32:11 +0200

    fix coords

--- game/model/map/generator/Room.java
+++ game/model/map/generator/Room.java
@@ -9,7 +9,7 @@ import model.map.Map;
 public class Room {
 
 	Model model;
-	
+
 	private int upperLeftX, upperLeftY;
 	private int width, height;
 
@@ -19,7 +19,7 @@ public class Room {
 
 	public Room (Model m, int w, int h, Case[][] composition, int typeID) {
 		model = m;
-		
+
 		upperLeftX = -1; //On remplie à -1 parce que l'emplacement dès salle sera fait plus tard
 		upperLeftY = -1;
 		width = w;
@@ -57,8 +57,8 @@ public class Room {
 				int entityID = c.getType().getSpawnerID();
 
 				if (entityID != -1) {
-					int x = i + upperLeftX - m.getWidth()/2;
-					int y = j + upperLeftY - m.getHeight()/2;
+					int x = i + upperLeftX;
+					int y = j + upperLeftY;
 					Entity e = model.createEntity(x, y, entityID);
 					//à enelever plus tard 
 					model.createLightSource(e);
