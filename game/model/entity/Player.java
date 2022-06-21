c8c2be2bf445b51f393915beb1bc0f7ef27ccfc9
delast <tanguy.delas@etu.univ-grenoble-alpes.fr>
2022-06-21 15:43:22 +0200

    lock déplacement par camera ready

--- game/model/entity/Player.java
+++ game/model/entity/Player.java
@@ -2,6 +2,7 @@ package model.entity;
 
 import model.Camera;
 
+
 public abstract class Player extends Entity {
 
 	public Player (double x, double y, EntityProperties ep) {
@@ -15,20 +16,38 @@ public abstract class Player extends Entity {
 		m_automata.step();
 		double speedX = super.m_vecDir.getX() * ENTITY_MAX_SPEED;
 		double speedY = super.m_vecDir.getY() * ENTITY_MAX_SPEED;
-		
+
 		if (Camera.getBlock()) {
 			Entity autreJ = autreJ();
 			double m_angle = m_vecDir.getAngle();
-			if (m_angle < Math.PI && autreJ.m_hitbox.getY() < m_hitbox.getY())
+			System.out.println(m_angle);
+			System.out.println("autre joueur Y " + autreJ.m_hitbox.getY());
+
+			//haut
+			if (m_angle < Math.PI && autreJ.m_hitbox.getY() > m_hitbox.getY()) {
+				System.out.println("détecté comme error haut");
 				return;
-			if (m_angle > Math.PI && autreJ.m_hitbox.getY() > m_hitbox.getY())
+			}
+
+			//bas
+			if (m_angle > Math.PI && autreJ.m_hitbox.getY() < m_hitbox.getY()) {
+				System.out.println("détecté comme error bas");
 				return;
-			if (m_angle > Math.PI/2 && m_angle < 3*Math.PI/2  && autreJ.m_hitbox.getX() > m_hitbox.getX())
+			}
+
+			//gauche
+			if (m_angle > Math.PI / 2 && m_angle < 3 * Math.PI / 2 && autreJ.m_hitbox.getX() > m_hitbox.getX()) {
+				System.out.println("détecté comme error gauche");
 				return;
-			if ((m_angle < Math.PI/2 || m_angle > 3*Math.PI/2) && autreJ.m_hitbox.getX() < m_hitbox.getX())
+			}
+
+			//droite
+			if ((m_angle < Math.PI / 2 || m_angle > 3 * Math.PI / 2) && autreJ.m_hitbox.getX() < m_hitbox.getX()) {
+				System.out.println("détecté comme error droite");
 				return;
+			}
 		}
-		
+
 		m_hitbox.move(speedX * elapsed / 1000, speedY * elapsed / 1000);
 		if (this.equals(torch.porteur))
 			torch.update(this);
