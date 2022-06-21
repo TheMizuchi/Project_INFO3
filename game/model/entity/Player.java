2538fa9b2cac68d07639f3ae004e2b7487c9c9ce
delast <tanguy.delas@etu.univ-grenoble-alpes.fr>
2022-06-21 14:56:35 +0200

    à merge avec clément

--- game/model/entity/Player.java
+++ game/model/entity/Player.java
@@ -1,5 +1,7 @@
 package model.entity;
 
+import model.Camera;
+
 public abstract class Player extends Entity {
 
 	public Player (double x, double y, EntityProperties ep) {
@@ -13,9 +15,30 @@ public abstract class Player extends Entity {
 		m_automata.step();
 		double speedX = super.m_vecDir.getX() * ENTITY_MAX_SPEED;
 		double speedY = super.m_vecDir.getY() * ENTITY_MAX_SPEED;
+		
+		
+		if (Camera.getBlock()) {
+			Entity autreJ = autreJ();
+			double m_angle = m_vecDir.getAngle();
+			if (m_angle < Math.PI && autreJ.m_hitbox.getY() < m_hitbox.getY())
+				return;
+			if (m_angle > Math.PI && autreJ.m_hitbox.getY() < m_hitbox.getY())
+				return;
+			if (m_angle > Math.PI/2 && m_angle < 3*Math.PI/2  && autreJ.m_hitbox.getX() > m_hitbox.getX())
+				return;
+			if ((m_angle < Math.PI/2 || m_angle > 3*Math.PI/2) && autreJ.m_hitbox.getX() < m_hitbox.getX())
+				return;
+		}
+		
 		m_hitbox.move(speedX * elapsed / 1000, speedY * elapsed / 1000);
 		if (this.equals(torch.porteur))
 			torch.update(this);
+	}
+
+	private Entity autreJ () {
+		if (this == J1.getInstance())
+			return J2.getInstance();
+		return J1.getInstance();
 	}
 
 	@Override
