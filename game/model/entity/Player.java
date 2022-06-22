06185faa61883a7502dc34d462511cdb77120fef
Diego CORREIA DE OLIVEIRA <Diego.cdo27@gmail.com>
2022-06-22 16:02:44 +0200

    Ajout du slow quand on porte la torche

--- game/model/entity/Player.java
+++ game/model/entity/Player.java
@@ -10,6 +10,7 @@ public abstract class Player extends Entity {
 
 	protected static final long POSSESSION_CD = 30;
 
+	final static double SLOW_TORCHE = 0.20;
 	final static double POSSESSION_RANGE = 10;
 	long m_possessionCD;
 	Mob m_possessing;
@@ -26,8 +27,17 @@ public abstract class Player extends Entity {
 			Torch torch = Torch.getInstance();
 			// déplacement
 			m_automata.step();
-			double speedX = super.m_vecDir.getX() * ENTITY_MAX_SPEED;
-			double speedY = super.m_vecDir.getY() * ENTITY_MAX_SPEED;
+
+			double speedX;
+			double speedY;
+
+			if (this.equals(torch.porteur)) {
+				speedX = super.m_vecDir.getX() * ENTITY_MAX_SPEED * (1 - SLOW_TORCHE);
+				speedY = super.m_vecDir.getY() * ENTITY_MAX_SPEED * (1 - SLOW_TORCHE);
+			} else {
+				speedX = super.m_vecDir.getX() * ENTITY_MAX_SPEED;
+				speedY = super.m_vecDir.getY() * ENTITY_MAX_SPEED;
+			}
 
 			if (Camera.getBlock()) {
 				Entity autreJ = autreJ();
