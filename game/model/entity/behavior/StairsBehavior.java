3ac0cf68bc8e3ccb036ae0fffda0a87d723d835d
Michelle Maufrand <michelle.maufrand@etu.univ-grenoble-alpes.fr>
2022-06-23 15:29:20 +0200

    cadeau trois nouvelles entity behavior pour le prix d'une

---
+++ game/model/entity/behavior/StairsBehavior.java
@@ -0,0 +1,16 @@
+package model.entity.behavior;
+
+import model.entity.Entity;
+import view.graphicEntity.StairsView;
+
+
+public class StairsBehavior extends EntityBehavior {
+
+	StairsView m_sv;
+
+
+	public StairsBehavior (Entity e, StairsView ev) {
+		super(e, ev);
+		m_sv = ev;
+	}
+}
