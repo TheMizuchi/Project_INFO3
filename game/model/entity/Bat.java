bd0ba9c3974a8f4fb2b4300c4edf33826d16504e
Diego CORREIA DE OLIVEIRA <Diego.cdo27@gmail.com>
2022-06-23 16:56:27 +0200

    Implémentation BatSpawner

--- game/model/entity/Bat.java
+++ game/model/entity/Bat.java
@@ -7,6 +7,7 @@ import view.graphicEntity.BatView;
 public class Bat extends Mob {
 
 	BatView m_bv;
+	BatSpawner m_spawner;
 
 
 	public Bat (double x, double y) {
@@ -14,11 +15,22 @@ public class Bat extends Mob {
 		m_bv = new BatView(this);
 		m_ev = m_bv;
 		MyCanvas.getInstance().createEntityView(m_bv);
+		m_tangible = false;
 	}
 
 	//Constructeur pour créer entité sans view
 	public Bat (double x, double y, Object o) {
 		super(x, y, EntityProperties.BAT);
+	}
+
+	void setSpawner (BatSpawner spawner) {
+		m_spawner = spawner;
+	}
+
+	@Override
+	public void deleteEntity () {
+		super.deleteEntity();
+		m_spawner.batDied();
 	}
 
 }
